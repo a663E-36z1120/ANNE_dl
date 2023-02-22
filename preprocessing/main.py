dc12694ea29a84cbea130fad9b92f51f66533608
a663E-36z1120 <79075841+a663E-36z1120@users.noreply.github.com>
2023-02-22 00:15:19 -0500

    functions for extracting data tensors and visualizing 30s windows complete

--- preprocessing/main.py
+++ preprocessing/main.py
@@ -7,27 +7,55 @@ from matplotlib import pyplot as plt
 
 DATA_DIR = "/mnt/Common/Data"
 SAMP_RATE = 25  # Hz
+WINDOW_LEN = 30  # Seconds
 
 
 def integrate_data(subject_id):
+    """ Prepares the data tensor and target vector of subject with subject_id
+    :param subject_id: the id of the subject for whom the data tensor and target will be prepared
+    :return: X: 3d tensor in the shape of (n, s, c), where:
+    n is the number of 30-second windows in the time-series data set
+    s the number of signal samples in each 30-second window (s = sample_rate * 30 seconds)
+    c is the number of signal sources, preliminarirly:
+    0: ECG  1: PPG  2,3,4: x,y,z accelerometer  5: ENMO 6: z-angle  7: temperature
+    """
     # Read raw signals from csv
-    index, time, ecg, ppg, x_acc, y_acc, z_acc, enmo, z_angle, \
+    index, time, ecg, ppg, x_acc, y_acc, z_acc, enmo, z_angle, temp \
         = np.loadtxt(f"{DATA_DIR}/{subject_id}/ANNE.csv", delimiter=",", dtype="float",
                      skiprows=1, unpack=True,
-                     usecols=(0, 1, 2, 3, 4, 5, 6, 7, 8))
+                     usecols=(0, 1, 2, 3, 4, 5, 6, 7, 8, 9))
+    n = len(ecg) // (SAMP_RATE*WINDOW_LEN) * (SAMP_RATE*WINDOW_LEN)
 
-    # Read additional features from edf
-    signals_psg, signal_headers_psg, header_psg = highlevel.read_edf(f"{DATA_DIR}/{subject_id}/PSG.edf")
-    ecg_psg = signals_psg[7]
+    # Features
+    ecg = np.reshape(ecg[:n], (-1, SAMP_RATE*WINDOW_LEN, 1))
+    ppg = np.reshape(ppg[:n], (-1, SAMP_RATE*WINDOW_LEN, 1))
+    x_acc = np.reshape(x_acc[:n], (-1, SAMP_RATE*WINDOW_LEN, 1))
+    y_acc = np.reshape(y_acc[:n], (-1, SAMP_RATE*WINDOW_LEN, 1))
+    z_acc = np.reshape(z_acc[:n], (-1, SAMP_RATE*WINDOW_LEN, 1))
+    enmo = np.reshape(enmo[:n], (-1, SAMP_RATE*WINDOW_LEN, 1))
+    z_angle = np.reshape(z_angle[:n], (-1, SAMP_RATE*WINDOW_LEN, 1))
+    temp = np.reshape(temp[:n], (-1, SAMP_RATE*WINDOW_LEN, 1))
 
-    ns = 10000
+    X = np.concatenate((ecg, ppg, x_acc, y_acc, z_acc, enmo, z_angle, temp), axis=2)
 
-    plt.plot(np.linspace(0,ns,25*ns), ecg[:ns*25], alpha=0.5)
-    plt.plot(np.linspace(0, ns, 256 * ns), ecg_psg[:ns * 256], alpha=0.5)
+    # targets
+    t = np.loadtxt(f"{DATA_DIR}/{subject_id}/PSG_score.txt", dtype="str")
+    t = np.reshape(t, (-1, len(t)))
+
+    return X, t
+
+
+def plot_window(X, index=0):
+    """
+    Plot the index-th 30 second window in the data tensor X
+    """
+    fig, axs = plt.subplots(X.shape[-1])
+    for i in range(len(axs)):
+        arr = X[index, :, i]
+        axs[i].plot(arr)
     plt.show()
-
-    pass
 
 
 if __name__ == "__main__":
-    integrate_data(132)
+    X, t = integrate_data(132)
+    plot_window(X, index=80)
