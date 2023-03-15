4fae19014996e5764d7529c202c9a021899ce25b
a663E-36z1120 <79075841+a663E-36z1120@users.noreply.github.com>
2023-03-15 05:52:53 -0400

    data integration not contains fine alignment

--- preprocessing/main.py
+++ preprocessing/main.py
@@ -4,15 +4,18 @@
 import numpy as np
 from pyedflib import highlevel
 from matplotlib import pyplot as plt
+from math import ceil
 
 DATA_DIR = "/mnt/Common/Data"
 SAMP_RATE = 25  # Hz
 WINDOW_LEN = 30  # Seconds
 
 
-def integrate_data(subject_id):
+def integrate_data(subject_id, shift=0):
     """ Prepares the data tensor and target vector of subject with subject_id
     :param subject_id: the id of the subject for whom the data tensor and target will be prepared
+    :param shift: shift of the ANNE signal in seconds, -: to the right, +: to the left
+
     :return: X: 3d tensor in the shape of (n, s, c), where:
     n is the number of 30-second windows in the time-series data set
     s the number of signal samples in each 30-second window (s = sample_rate * 30 seconds)
@@ -26,7 +29,35 @@ def integrate_data(subject_id):
                      usecols=(0, 1, 2, 3, 4, 5, 6, 7, 8, 9))
     n = len(ecg) // (SAMP_RATE*WINDOW_LEN) * (SAMP_RATE*WINDOW_LEN)
 
-    plot_ecg(ecg, subject_id)
+    # targets
+    t = np.loadtxt(f"{DATA_DIR}/{subject_id}/PSG_score.txt", dtype="str")
+
+
+    # shift according to alignment
+    if shift < 0:
+        n_windows_left = ceil(abs(shift / WINDOW_LEN))
+        n_samples_right = ceil(abs(shift * SAMP_RATE))
+        n_samples_left = (n - n_samples_right) % (WINDOW_LEN * SAMP_RATE)
+        t = t[n_windows_left:]
+
+        # plot_ecg(ecg, subject_id, windows_dropped=n_windows_left)
+    else:
+        n_windows_right = ceil(abs(shift / WINDOW_LEN))
+        n_samples_left = ceil(abs(shift * SAMP_RATE))
+        n_samples_right = (n - n_samples_left) % (WINDOW_LEN * SAMP_RATE)
+        t = t[:-n_windows_right]
+
+    ecg = ecg[n_samples_left:-n_samples_right]
+    ppg = ppg[n_samples_left:-n_samples_right]
+    x_acc = x_acc[n_samples_left:-n_samples_right]
+    y_acc = y_acc[n_samples_left:-n_samples_right]
+    z_acc = z_acc[n_samples_left:-n_samples_right]
+    enmo = enmo[n_samples_left:-n_samples_right]
+    z_angle = z_angle[n_samples_left:-n_samples_right]
+    temp = temp[n_samples_left:-n_samples_right]
+
+    n = n - n_samples_left - n_samples_right
+
 
     # Features
     ecg = np.reshape(ecg[:n], (-1, SAMP_RATE*WINDOW_LEN, 1))
@@ -38,17 +69,14 @@ def integrate_data(subject_id):
     z_angle = np.reshape(z_angle[:n], (-1, SAMP_RATE*WINDOW_LEN, 1))
     temp = np.reshape(temp[:n], (-1, SAMP_RATE*WINDOW_LEN, 1))
 
+    t = np.reshape(t, (-1, len(t)))
     X = np.concatenate((ecg, ppg, x_acc, y_acc, z_acc, enmo, z_angle, temp), axis=2)
 
-    # targets
-    t = np.loadtxt(f"{DATA_DIR}/{subject_id}/PSG_score.txt", dtype="str")
-    t = np.reshape(t, (-1, len(t)))
 
     return X, t
 
-t = 1200
 
-def plot_ecg(anne, subject_id, start=8440, stop=8480, anne_shift=-7):
+def plot_ecg(anne, subject_id, start=0, stop=4200, anne_shift=0, windows_dropped=0):
     """
     Plot ANNE ecg signal against PSG ecg signals for visualizing the result of alignment
     """
@@ -60,14 +88,12 @@ def plot_ecg(anne, subject_id, start=8440, stop=8480, anne_shift=-7):
     fig, axs = plt.subplots(2)
     axs[0].plot(np.linspace(start, stop, (stop-start)*SAMP_RATE), anne[(start+anne_shift)*SAMP_RATE:(stop+anne_shift)*SAMP_RATE])
     axs[0].title.set_text("ANNE")
-    axs[1].plot(np.linspace(start, stop, (stop-start)*psg_samp_rate), signals[7][start*psg_samp_rate:stop*psg_samp_rate])
+    axs[1].plot(np.linspace(start, stop, (stop-start)*psg_samp_rate), signals[7][(start+windows_dropped*WINDOW_LEN)*psg_samp_rate:(stop+windows_dropped*WINDOW_LEN)*psg_samp_rate])
     axs[1].title.set_text("PSG")
 
     plt.show()
 
     pass
-
-
 
 
 def plot_window(X, index=0):
@@ -82,5 +108,5 @@ def plot_window(X, index=0):
 
 
 if __name__ == "__main__":
-    X, t = integrate_data(132)
+    X, t = integrate_data(288, -38.36)
     plot_window(X, index=80)
