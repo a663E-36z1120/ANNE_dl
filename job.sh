7732948fc8ebbad660c48d32ae79b5f6ec19f842
Chunlin Li <ed11235@cedar1.cedar.computecanada.ca>
2023-10-04 19:39:37 -0700

    rewrite job.sh so that it automatically create python environment

--- job.sh
+++ job.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
-#
-#SBATCH --nodes=1
-#SBATCH --tasks-per-node=1
-#SBATCH --gres=gpu:1
-#SBATCH --cpus-per-task=1
-#SBATCH --mem=32000M
+#SBATCH --nodes 1
+#SBATCH --gres=gpu:1 # request a GPU
+#SBATCH --tasks-per-node=1 
+#SBATCH --cpus-per-task=1 # change this parameter to 2,4,6,... and increase "--num_workers" accordingly to see the effect on performance
+#SBATCH --mem=8G      
 #SBATCH --time=2:00:00
-#SBATCH --account=def-alim
 #SBATCH --output=output_%j.out
 #SBATCH --error=error_%j.out
+#SBATCH --account=def-alim
 
-module load python
-module load scipy-stack
-
-source ./pytorchENV/bin/activate
-# pip uninstall numpy -y
+module load python # Using Default Python version - Make sure to choose a version that suits your application
+virtualenv --no-download $SLURM_TMPDIR/env
+source $SLURM_TMPDIR/env/bin/activate
+pip install torch torchvision --no-index
 pip install --no-index numpy==1.25.0
 pip install matplotlib --no-index
 pip install scipy --no-index
-pip install -r requirements.txt
 
 cd /home/$USER/projects/def-alim/$USER/ANNE_dl
+pip install -r requirements.txt
+python -u ./models/train.py
+
 python -u ./models/train.py
